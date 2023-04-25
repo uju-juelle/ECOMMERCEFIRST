757775729bdb62ebaebebd596427d95638429d0d
ujunwa <ujmarylee@gmail.com>
2023-04-25 13:49:33 -0700

    initial

--- money/models.py
+++ money/models.py
@@ -15,3 +15,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Product(models.Model):
+   name = models.CharField(max_length=100)
+   description = models.TextField()
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   category = models.ForeignKey(Category,on_delete=models.CASCADE)
+   image = models.ImageField(upload_to="product_images")
+
+
+   def __str__(self):
+      return self.name
