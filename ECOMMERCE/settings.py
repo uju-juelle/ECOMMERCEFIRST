9fb40c4b00da2f9370dd10d1d40c46be1b399788
ujunwa <ujmarylee@gmail.com>
2023-04-25 11:48:49 -0700

    initial

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for ECOMMERCE project.
 
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- money/models.py
+++ money/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="category_images")
+
+
+    def __str__(self):
+        return self.name
